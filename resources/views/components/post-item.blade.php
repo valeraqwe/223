d135d910ec8c56ff31a4bc76838cf9c83bb27f56
valeraqwe <valera.komar.1999@gmail.com>
2023-04-11 11:15:07 +0300

    implement pagination

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -1,20 +1,21 @@
 <article class="flex flex-col shadow my-4">
     <!-- Article Image -->
     <a href="#" class="hover:opacity-75">
-        <img src="/storage/{{$post->thumbnail}}">
+        <img src="{{$post->getThumbnail()}}">
     </a>
     <div class="bg-white flex flex-col justify-start p-6">
         @foreach($post->categories as $category)
-
+            <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">
+                {{$category->title}}
+            </a>
         @endforeach
-        <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">
-            {{$category->title}}
-        </a>
+
         <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">
             {{$post->title}}
         </a>
         <p href="#" class="text-sm pb-3">
-            By <a href="#" class="font-semibold hover:text-gray-800">{{$post->user->name}}</a>, Published on
+            By <a href="#" class="font-semibold hover:text-gray-800">{{$post->user->name}}</a>,
+            Published on
             {{$post->getFormattedDate()}}
         </p>
         <a href="#" class="pb-6">
