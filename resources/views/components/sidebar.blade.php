49656e3d97381d7595502732f618a8d14f921af1
valeraqwe <valera.komar.1999@gmail.com>
2023-04-24 09:40:41 +0300

    about us page

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -5,7 +5,7 @@
         <h3 class="text-xl font-semibold mb-3">All Categories</h3>
         @foreach($categories as $category)
             <a href="{{route('by-category', $category)}}"
-               class="text-semibold block py-2 px-3 rounded {{request('category')->slug === $category->slug
+               class="text-semibold block py-2 px-3 rounded {{ request('category')?->slug === $category->slug
                 ? 'bg-blue-600 text-white' : ''}}">
                 {{$category->title}} ({{$category->total}})
             </a>
