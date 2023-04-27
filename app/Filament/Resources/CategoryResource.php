5acf718ad88b9f626ff843e786276a8322b5cc79
valeraqwe <valera.komar.1999@gmail.com>
2023-04-27 13:54:39 +0300

    search in posts and categories

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -43,8 +43,10 @@ class CategoryResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('title')->searchable()
+                    ->sortable(),
                 Tables\Columns\TextColumn::make('updated_at')
+                    ->sortable()
                     ->dateTime(),
             ])
             ->filters([
