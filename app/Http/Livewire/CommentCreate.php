ae8d543378d6df0363677c29174388090442c66b
valeraqwe <valera.komar.1999@gmail.com>
2023-05-11 17:35:02 +0300

    deleting and editing comments

--- app/Http/Livewire/CommentCreate.php
+++ app/Http/Livewire/CommentCreate.php
@@ -12,9 +12,13 @@ class CommentCreate extends Component
 
     public Post $post;
 
-    public function mount(Post $post)
+    public ?Comment $commentModel = null;
+
+    public function mount(Post $post, $commentModel = null)
     {
         $this->post = $post;
+        $this->commentModel = $commentModel;
+        $this->comment = $commentModel ? $commentModel->comment : '';
     }
 
     public function render()
@@ -28,13 +32,26 @@ class CommentCreate extends Component
         if (!$user) {
             return $this->redirect('/login');
         }
-        $comment = Comment::create([
-            'comment' => $this->comment,
-            'post_id' => $this->post->id,
-            'user_id' => $user->id
-        ]);
 
-        $this->emitUp('commentCreated', $comment->id);
-        $this->comment = '';
+        if ($this->commentModel) {
+            if ($this->commentModel->user_id != $user->id) {
+                return response('You are not allowed to perform this action', 403);
+            }
+
+            $this->commentModel->comment = $this->comment;
+            $this->commentModel->save();
+
+            $this->comment = '';
+            $this->emitUp('commentUpdated');
+        } else {
+            $comment = Comment::create([
+                'comment' => $this->comment,
+                'post_id' => $this->post->id,
+                'user_id' => $user->id
+            ]);
+
+            $this->emitUp('commentCreated', $comment->id);
+            $this->comment = '';
+        }
     }
 }
--- app/Http/Livewire/CommentItem.php
+++ app/Http/Livewire/CommentItem.php
@@ -9,6 +9,13 @@ class CommentItem extends Component
 {
     public Comment $comment;
 
+    public bool $editing = false;
+
+    protected $listeners = [
+        'cancelEditing' => 'cancelEditing',
+        'commentUpdated' => 'commentUpdated',
+    ];
+
     public function mount(Comment $comment)
     {
         $this->comment = $comment;
@@ -21,8 +28,33 @@ class CommentItem extends Component
 
     public function deleteComment()
     {
+        $user = auth()->user();
+        if (!$user) {
+            return $this->redirect('/login');
+        }
+
+        if ($this->comment->user_id != $user->id) {
+            return response('You are not allowed to perform this action', 403);
+        }
+
         $id = $this->comment->id;
+
         $this->comment->delete();
         $this->emitUp('commentDeleted', $id);
+    }
+
+    public function startCommentEdit()
+    {
+        $this->editing = true;
+    }
+
+    public function cancelEditing()
+    {
+        $this->editing = false;
+    }
+
+    public function commentUpdated()
+    {
+        $this->editing = false;
     }
 }
